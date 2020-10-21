1ec4c282dffc40043b8cee17351f63f484951451
royhuang <hyfdudu@gmail.com>
2020-10-20 22:15:56 -0700

    added a few more commands

--- covid_databot/databot2.py
+++ covid_databot/databot2.py
@@ -1,10 +1,10 @@
 import discord
+from discord.ext import commands
 import requests
 import json
 import os
 from random import seed
 from random import random
-from dotenv import load_dotenv
 from decouple import config
 
 # establish client connection
@@ -12,6 +12,7 @@ TOKEN = config("TOKEN")
 print(TOKEN)
 
 client = discord.Client()
+bot = commands.Bot(command_prefix='covid', description='Hi')
 seed(4)
 # get ISO 2-digit country codes from JSON
 country_codes_raw = json.load(open("iso_country_codes.json"))
@@ -28,10 +29,13 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    #await bot.change_presence(activity=discord.Game(name="type \"covid help\""))
 
 
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
     if message.content == 'covid help':
         help_msg = discord.Embed(
             title = 'Useful Commands',
@@ -41,20 +45,33 @@ async def on_message(message):
         help_msg.set_footer(text='Data from: https://about-corona.net/')
         help_msg.add_field(name='Command',
                             value='help\n'+
-                                  'stats total_cases <country>\n'+
-                                  'stats total_deaths <country>',
+                                  'stats <country>\n'+
+                                  'has_covid <name>\n'+
+                                  'miracle_cure',
                             inline=True)
         help_msg.add_field(name='Description',
                             value='This command\n'+
-                                  'Gets total confirmed cases in <country>\n'+
-                                  'Gets total confiremd deaths in <country>', 
+                                  'Gets stats for <country>\n'+
+                                  'Tests <name> for COVID-19\n'+
+                                  'Gets rid of all active COVID-19 cases', 
                             inline=True)
         await message.channel.send(embed=help_msg)
 
     if message.content == 'covid country_list':
-        await message.channel.send("to be implemented soon in embed mode")
+        country_list_raw = list(name_code_pair.keys())[:10]
+        country_list_str = ''
+        for c in country_list_raw:
+            country_list_str += c + '\n'
+        lst_embed = discord.Embed(
+            title='Country List',
+            description='Here is a list of countries that work with this bot',
+            colour=discord.Colour.red()
+        )
+        lst_embed.add_field(name='Names',value=country_list_str)        
+        await message.channel.send(embed=lst_embed)
+
     if message.content.startswith('covid stats'):
-        req_country = message.content.split()[2]
+        req_country = " ".join([country.title() for country in message.content.split()[2:]])
         r = requests.get('https://corona-api.com/countries/' + name_code_pair[req_country])
         json_data = json.loads(r.text)['data']
         cases = json_data['latest_data']['confirmed']
@@ -92,5 +109,21 @@ async def on_message(message):
     if message.content.startswith('covid miracle_cure'):
         infected = []
         await message.channel.send("All cases of covid has been cured...for now hehehehe")
+
+    if message.content.startswith('covid avatar'):
+        if message.mentions.__len__() > 0:
+            for user in message.mentions:
+                await message.channel.send(user.avatar_url)
+
+    if message.content.startswith('covid rate'):
+        rated = " ".join(message.content.split()[2:])
+        result = (sum([ord(rated[i]) * 9 * (i+1)
+                       for i in range(len(rated))]) * 42 // 69) % 11
+        emoji = "👍" if result >= 5 else "👎"
+        emb = discord.Embed(
+            description=f"I rate ***{rated}*** a **{result}/10** {emoji}",
+            colour=discord.Colour.red()
+        )
+        await message.channel.send(embed=emb)
 
 client.run(TOKEN)
